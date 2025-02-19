3a2a7246bfa9ed6f778c2b41f40f9df098d2860e
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-20 02:35:58 +0700

    Task4.6

--- src/service/EventService.ts
+++ src/service/EventService.ts
@@ -1,22 +1,24 @@
 import type { Event } from "../models/Event";
-import {
-    getAllEvents as allEvents, getEventById as eventById, addEvent as addNewEvent
-} from "../repository/EventRepository";
+import * as repo from "../repository/EventRepository";
 
 
 export function getEventByCategory(category: string): Promise<Event[]> {
-    return getEventByCategory(category);
+    return repo.getEventByCategory(category);
 }
+
 
 
 export function getAllEvents(): Promise<Event[]> {
-    return allEvents();
+    return repo.getAllEvents();
+
 }
 
 export function getEventById(id: number): Promise<Event | undefined> {
-    return eventById(id);
+    return repo.getEventById(id);
+
 }
 
 export function addEvent(newEvent: Event): Promise<Event> {
-    return addNewEvent(newEvent);
+    return repo.addEvent(newEvent);
+
 }
