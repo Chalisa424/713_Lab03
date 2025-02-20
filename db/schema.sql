5055908b30ee6b7a7e1d1c7fa3bebee911e4a7da
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-20 23:57:10 +0700

    Task6.3

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,16 @@ CREATE TABLE events (
     petsAllowed BOOLEAN,
     organizer VARCHAR(255)
 );
+
+CREATE TABLE books (
+    id INT PRIMARY KEY,
+    category VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    location VARCHAR(255),
+    date DATE,
+    time TIME,
+    petsAllowed BOOLEAN,
+    organizer VARCHAR(255)
+);
+
--- db/seed.sql
+++ db/seed.sql
@@ -5,3 +5,12 @@ INSERT INTO events (id, category, title, description, location, date, time, pets
 (4, 'Sports', 'Basketball Game', 'A basketball game between two rival teams.', 'National Basketball Court', '2025-06-20', '19:30', FALSE, 'National Sports League'),
 (5, 'Technology', 'Tech Conference', 'A conference on the latest trends in technology.', 'Convention Center', '2025-07-10', '09:00', FALSE, 'Tech Innovators Group'),
 (6, 'Food', 'Food Festival', 'A festival featuring various local and international cuisines.', 'City Park', '2025-08-15', '12:00', TRUE, 'Gourmet Events');
+
+
+INSERT INTO books (id, category, title, description, location, date, time, petsAllowed, organizer)VALUES
+(1, 'Group1', 'Fullmetal Alchemist', 'Edward and Alphonse Elric break alchemy\'s ultimate taboo—human transmutation...', 'Tokyo', '2025-02-20', '14:30:00', TRUE, 'Hiromu Arakawa'),
+(2, 'Group2', 'Attack on Titan', 'In a world where humanity resides within enormous walled cities...', 'Osaka', '2025-02-22', '15:00:00', FALSE, 'Hajime Isayama'),
+(3, 'Group3', 'Death Note', 'A high school student, Light Yagami, discovers a mysterious notebook...', 'Kyoto', '2025-03-01', '16:00:00', TRUE, 'Tsugumi Ohba'),
+(4, 'Group1', 'One Piece', 'Monkey D. Luffy, a young pirate with the ability to stretch...', 'Fukuoka', '2025-03-05', '17:00:00', TRUE, 'Eiichiro Oda'),
+(5, 'Group2', 'Naruto', 'Naruto Uzumaki, an orphaned ninja, dreams of becoming the strongest...', 'Nagoya', '2025-03-10', '18:00:00', FALSE, 'Masashi Kishimoto'),
+(6, 'Group3', 'Dragon Ball', 'The adventures of Goku, a powerful martial artist...', 'Sapporo', '2025-03-12', '19:00:00', TRUE, 'Akira Toriyama');
